fc018c95efdd77d4d96337aedca7ef23ef36875b
Andrew Vos <andrew.vos@gmail.com>
2012-06-20 22:44:50 +0100

    rewrite the script to work with newer Arduino.app

--- example1/example.pde
+++ example1/example.pde
@@ -1,12 +1,14 @@
-int ledPin = 13;
+#include "Arduino.h"
 
-void setup(){
-  pinMode(ledPin, OUTPUT);
+int led = 13;
+
+void setup() {
+  pinMode(led, OUTPUT);
 }
 
-void loop(){
-  digitalWrite(ledPin, HIGH);
-  delay(100);
-  digitalWrite(ledPin, LOW);
-  delay(100);
+void loop() {
+  digitalWrite(led, HIGH);
+  delay(1000);
+  digitalWrite(led, LOW);
+  delay(1000);
 }
--- rakefile.rb
+++ rakefile.rb
@@ -1,44 +1,41 @@
 require 'tempfile'
 require 'fileutils'
 
-PROJECT          ||= File.basename(Dir.glob("*.pde").first, ".pde")
-MCU              ||= 'atmega328p'
-CPU              ||= '16000000L'
-PORT             ||= Dir.glob('/dev/tty.usbmodem*').first
-BITRATE          ||= '115200'
-PROGRAMMER       ||= 'stk500v1'
+PROJECT          = File.basename(Dir.glob("*.pde").first, ".pde")
+MCU              = 'atmega328p'
+CPU              = '16000000L'
+PORT             = Dir.glob('/dev/tty.usbmodem*').first
+BITRATE          = '115200'
 
-BUILD_OUTPUT     ||= 'build'
-ARDUINO_HARDWARE ||= '/Applications/Arduino.app/Contents/Resources/Java/hardware'
-AVRDUDE          ||= "#{ARDUINO_HARDWARE}/tools/avr/bin/avrdude"
-AVRDUDE_CONF     ||= "#{ARDUINO_HARDWARE}/tools/avr/etc/avrdude.conf"
-ARDUINO_CORES    ||= "#{ARDUINO_HARDWARE}/arduino/cores/arduino"
-AVR_G_PLUS_PLUS  ||= "#{ARDUINO_HARDWARE}/tools/avr/bin/avr-g++"
-AVR_GCC          ||= "#{ARDUINO_HARDWARE}/tools/avr/bin/avr-gcc"
-AVR_AR           ||= "#{ARDUINO_HARDWARE}/tools/avr/bin/avr-ar"
-AVR_OBJCOPY      ||= "#{ARDUINO_HARDWARE}/tools/avr/bin/avr-objcopy"
+BUILD_OUTPUT     = 'build'
+ARDUINO_ROOT     = '/Applications/Arduino.app/Contents/Resources/Java'
+AVRDUDE          = "#{ARDUINO_ROOT}/hardware/tools/avr/bin/avrdude"
+AVRDUDE_CONF     = "#{ARDUINO_ROOT}/hardware/tools/avr/etc/avrdude.conf"
+
+AVR_G_PLUS_PLUS  = "#{ARDUINO_ROOT}/hardware/tools/avr/bin/avr-g++"
+AVR_GCC          = "#{ARDUINO_ROOT}/hardware/tools/avr/bin/avr-gcc"
+AVR_AR           = "#{ARDUINO_ROOT}/hardware/tools/avr/bin/avr-ar"
+AVR_OBJCOPY      = "#{ARDUINO_ROOT}/hardware/tools/avr/bin/avr-objcopy"
+ARDUINO_CORES    = "#{ARDUINO_ROOT}/hardware/arduino/cores/arduino"
+ARDUINO_VARIANTS = "#{ARDUINO_ROOT}/hardware/arduino/variants/standard"
 
 def build_output_path(file)
-  Dir.mkdir(BUILD_OUTPUT) if Dir.exist?(BUILD_OUTPUT) == false
-  File.join(BUILD_OUTPUT, file)
+  FileUtils.mkdir_p(BUILD_OUTPUT)
+  File.expand_path(File.join(BUILD_OUTPUT, file))
 end
 
-C_FILES          ||= Dir.glob("#{ARDUINO_CORES}/*.c")
-CPP_FILES        ||= Dir.glob("#{ARDUINO_CORES}/*.cpp") + [build_output_path("#{PROJECT}.cpp")]
+desc "build and upload"
+task :default => [:build, :upload]
 
-desc "Compile and upload"
-task :default => [:compile, :upload]
+desc "build the hex file"
+task :build => [:clean, :preprocess, :compile]
 
-desc "Compile the hex file"
-task :compile => [:clean, :preprocess, :c, :cpp, :hex]
-
-desc "Upload compiled hex file to your device"
+desc "upload hex file to your device"
 task :upload do
-  hex = build_output_path("#{PROJECT}.hex")
-  sh "#{AVRDUDE} -C#{AVRDUDE_CONF} -q -q -p#{MCU} -c#{PROGRAMMER} -P#{PORT} -b#{BITRATE} -D -Uflash:w:#{hex}:i"
+  sh "#{AVRDUDE} -C#{AVRDUDE_CONF} -v -v -v -v -p#{MCU} -carduino -P#{PORT} -b#{BITRATE} -D -Uflash:w:#{build_output_path("#{PROJECT}.cpp.hex")}:i"
 end
 
-desc "Delete the build output directory"
+desc "delete the build output directory"
 task :clean do
   FileUtils.rm_rf(BUILD_OUTPUT)
 end
@@ -47,38 +44,44 @@ task :preprocess do
   pde = "#{PROJECT}.pde"
   cpp = build_output_path("#{PROJECT}.cpp")
   File.open(cpp, 'w') do |file|
-    file.puts '#include "WProgram.h"'
     file.puts File.read(pde)
   end
 end
 
-task :c do
-  C_FILES.each do |source|
-    output = build_output_path(File.basename(source, File.extname(source)) + ".o")
-    sh "#{AVR_GCC} -c -g -Os -w -ffunction-sections -fdata-sections -mmcu=#{MCU} -DF_CPU=#{CPU} -DARDUINO=22 -I#{ARDUINO_CORES} #{source} -o#{output}"
+task :compile do
+  cpp = build_output_path("#{PROJECT}.cpp")
+  gplusplus cpp
+
+  Dir.glob("#{ARDUINO_ROOT}/hardware/arduino/cores/arduino/*.c").each do |c|
+    gcc c
   end
+  Dir.glob("#{ARDUINO_ROOT}/hardware/arduino/cores/arduino/*.cpp").each do |cpp|
+    gplusplus cpp
+  end
+  Dir.glob("#{ARDUINO_ROOT}/hardware/arduino/cores/arduino/*.cpp").each do |cpp|
+    gplusplus cpp
+  end
+
+  Dir.glob(build_output_path("*.o")).each do |o|
+    o o
+  end
+
+  sh "#{AVR_GCC} -Os -Wl,--gc-sections -mmcu=atmega328p -o #{build_output_path("#{PROJECT}.cpp.elf")} #{build_output_path("#{PROJECT}.cpp.o")} #{build_output_path("core.a")} -L#{build_output_path("#{PROJECT}.tmp")} -lm"
+
+  sh "#{AVR_OBJCOPY} -O ihex -j .eeprom --set-section-flags=.eeprom=alloc,load --no-change-warnings --change-section-lma .eeprom=0 #{build_output_path("#{PROJECT}.cpp.elf")} #{build_output_path("#{PROJECT}.cpp.eep")}"
+  sh "#{AVR_OBJCOPY} -O ihex -R .eeprom #{build_output_path("#{PROJECT}.cpp.elf")} #{build_output_path("#{PROJECT}.cpp.hex")}"
 end
 
-task :cpp do
-  CPP_FILES.each do |source|
-    output = build_output_path(File.basename(source, File.extname(source)) + ".o")
-    sh "#{AVR_G_PLUS_PLUS} -c -g -Os -w -fno-exceptions -ffunction-sections -fdata-sections -mmcu=#{MCU} -DF_CPU=#{CPU} -DARDUINO=22 -I#{ARDUINO_CORES} #{source} -o#{output}"
-  end
+def gplusplus source_file
+  output = build_output_path(File.basename(source_file) + ".o")
+  sh "#{AVR_G_PLUS_PLUS} -c -g -Os -Wall -fno-exceptions -ffunction-sections -fdata-sections -mmcu=#{MCU} -DF_CPU=#{CPU} -MMD -DUSB_VID=null -DUSB_PID=null -DARDUINO=101 -I#{ARDUINO_CORES} -I#{ARDUINO_VARIANTS} #{source_file} -o #{output}"
 end
 
-task :hex do
-  o       = build_output_path("#{PROJECT}.o")
-  elf     = build_output_path("#{PROJECT}.elf")
-  archive = build_output_path('core.a')
-  eep     = build_output_path("#{PROJECT}.eep")
-  hex     = build_output_path("#{PROJECT}.hex")
+def gcc source_file
+  output = build_output_path(File.basename(source_file) + ".o")
+  sh "#{AVR_GCC} -c -g -Os -Wall -ffunction-sections -fdata-sections -mmcu=#{MCU} -DF_CPU=#{CPU} -MMD -DUSB_VID=null -DUSB_PID=null -DARDUINO=101 -I#{ARDUINO_CORES} -I#{ARDUINO_VARIANTS} #{source_file} -o #{output}"
+end
 
-  (C_FILES + CPP_FILES).each do |file|
-    file = build_output_path(File.basename(file, File.extname(file)) + ".o")
-    sh "#{AVR_AR} rcs #{archive} #{file}"
-  end
-
-  sh "#{AVR_GCC} -Os -Wl,--gc-sections -mmcu=#{MCU} -o #{elf} #{o} #{archive} -L#{BUILD_OUTPUT} -lm"
-  sh "#{AVR_OBJCOPY} -O ihex -j .eeprom --set-section-flags=.eeprom=alloc,load --no-change-warnings --change-section-lma .eeprom=0 #{elf} #{eep}"
-  sh "#{AVR_OBJCOPY} -O ihex -R .eeprom #{elf} #{hex}"
+def o source_file
+  sh "#{AVR_AR} rcs #{build_output_path("core.a")} #{source_file}" 
 end
