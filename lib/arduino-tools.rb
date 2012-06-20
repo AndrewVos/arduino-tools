624b4a25984df8e5c97b22da8930cd532839ed08
Andrew Vos <andrew.vos@gmail.com>
2012-06-20 19:36:51 +0100

    Convert to a gem

---
+++ lib/arduino-tools.rb
@@ -0,0 +1,7 @@
+require "arduino-tools/version"
+
+module Arduino
+  module Tools
+    # Your code goes here...
+  end
+end
---
+++ lib/arduino-tools/version.rb
@@ -0,0 +1,5 @@
+module Arduino
+  module Tools
+    VERSION = "0.0.1"
+  end
+end
